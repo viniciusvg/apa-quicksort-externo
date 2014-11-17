221e16bdccfd59811925b4f542927a3f67c7508c
raphael <raphaelcampos.rp@gmail.com>
2014-11-17 16:45:48 -0200

    define TAM incluido

--- teste_interno.c
+++ teste_interno.c
@@ -5,19 +5,18 @@ de ordenação interna*/
 #include <stdlib.h>
 #include "vetor.h"
 #include "ordenacaoInterna.h"
+#define TAM 5
 
 int main(void)
 {
     
     int escolha = 0;
 
-    int vetor[5] = {9, 2, 3, 4, 11};
-    int cp_vetor[5];
-
-    int tam = 5;
+    int vetor[TAM] = {9, 2, 3, 4, 11};
+    int cp_vetor[TAM];
 
     printf("\nVetor a ser ordenado: ");
-    imprime_vetor(vetor, 5);
+    imprime_vetor(vetor, TAM);
 
     printf("\nEscolha o algoritmo de ordenacao a ser testado:\n");
     printf("1 - Todos\n");
@@ -32,12 +31,12 @@ int main(void)
         case 1:
             printf("todos.\n");
             
-            copia_vetor(vetor, cp_vetor, tam);
+            copia_vetor(vetor, cp_vetor, TAM);
             
-            quicksort(cp_vetor, 0, tam-1);
-            imprime_vetor(cp_vetor, tam);
+            quicksort(cp_vetor, 0, TAM-1);
+            imprime_vetor(cp_vetor, TAM);
 
-            if(verifica_ordem(cp_vetor, tam))
+            if(verifica_ordem(cp_vetor, TAM))
                 puts("Esta ordenado");
             else
                 puts("Vetor nao ordenado");
